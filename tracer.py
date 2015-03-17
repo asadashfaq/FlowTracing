650f67d3c9ed1c0bb85aad79e5d661641020ad45
Bo Tranberg <bo@tberg.dk>
2015-03-17 15:37:05 +0100

    multiple time stesp

--- tracer.py
+++ tracer.py
@@ -16,23 +16,33 @@ def nodeMix(F, K, P, t):
     F:  actual flows in the network
     K:  incidence matrix
     P:  injection pattern
-    t:  specifying which ours of the time series to sole
+    t:  specifying which ours of the time series to sole either an integer
+        or a list of time steps.
 
     Output:
     An n by n matrix with node n's export in row n and node n's import in
     column n.
     """
-
     K = matrix(K)
-    P = matrix(P[:, t])
-    I = identity(P.shape[1])
-    F = diagM(F[:, t])
+    dim = K.shape[0]
 
-    C = posM(P) * invert(I - negM(posM(K * F) * K.T))
+    if type(t) == int:
+        timeSteps = [t]
+        C = np.zeros((1, dim, dim))
+    elif type(t) == list:
+        timeSteps = t
+        C = np.zeros((len(t), dim, dim))
+
+    for i, t in enumerate(timeSteps):
+        P = diagM(P[:, t])
+        I = identity(dim)
+        F = diagM(F[:, t])
+        C[i] = posM(P) * invert(I - negM(posM(K * F) * K.T))
     return C
 
 # load test values
 F = np.load('./input/F.npy')
 K = np.load('./input/K.npy')
 P = np.load('./input/phi.npy')
-t = 0
+t = [0, 1]
+C = nodeMix(F, K, P, t)
